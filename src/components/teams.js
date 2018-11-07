fec2c58092e972c71bf0f45a1e1e7ec47e9522e9
Sam Dalton <spd4nf@virginia.edu>
2018-11-07 14:41:07 -0600

    ahhh

--- src/components/teams.js
+++ src/components/teams.js
@@ -3,21 +3,22 @@ import React from 'react';
 export default function Teams(props){
   props.list.sort((a,b)=>(a.fullName>b.fullName) ? 1 : (a.fullName<b.fullName) ?-1:0);
   const teams = props.list.map((team, index) => 
-    <li key={index} className="flip-teamcard">
-      <div className="flip-teamcard-inner">
-        <div className="flip-teamcard-front">
-          <strong>{team.fullName}</strong>
-          <img src={team.image} alt={team.nickName}/>
-          <address className="location">{team.location}</address>
-        </div>
-        <div className='flip-teamcard-back'>
-          <h1>Team Roster</h1>
-          <div className="roster">
-            SAMPLE
-          </div>
-        </div>
+
+  <li key={index} className="flip-teamcard">
+    <div className="flip-teamcard-inner">
+      <div class="flip-teamcard-front">
+        <strong>{team.fullName}</strong>
+        <img src={team.image} alt={team.nickName}/>
+        <address className="location">{team.location}</address>
       </div>
-    </li>
+      <div class="flip-teamcard-back">
+        <h1>John Doe</h1> 
+        <p>Architect & Engineer</p> 
+       <p>We love that guy</p>
+      </div>
+    </div>
+  </li>
+  
   );
   return (
     <ul className="team-list">
